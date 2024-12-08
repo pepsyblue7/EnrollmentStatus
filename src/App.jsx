32e2e0cadf9740fe98093505526b2523ef63169f
Your Name <pepsyblue7@gmail.com>
2024-12-08 23:58:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderNavbar from "./components/HeaderNavbar";
+import EnrollmentStatus from "./components/EnrollmentStatus";
+import "./styles/enrollmentstatus.css";
+
+function App() {
+  return (
+    <div>
+      <HeaderNavbar />
+      <EnrollmentStatus />
+    </div>
+  );
+}
+
+export default App;
